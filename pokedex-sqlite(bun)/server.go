0fd630cfcbf8b03d2060f483fea50fefb837349b
benz <benzchanapa@outlook.com>
2023-01-31 21:10:38 +0700

    change to gochi

--- pokedex-sqlite(bun)/server.go
+++ pokedex-sqlite(bun)/server.go
@@ -3,35 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 	"pokedex-bun/database"
 	"pokedex-bun/graph"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-	// database.ConnectDatabse()
-	srv := handler.NewDefaultServer(
-		graph.NewExecutableSchema(
-			graph.Config{
-				Resolvers: &graph.Resolver{
-					DB: database.ConnectDatabase(),
-				},
-			},
-		),
-	)
+	r := chi.NewRouter()
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: database.ConnectDatabase()}}))
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, r))
 }
